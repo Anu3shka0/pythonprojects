08e41b6c25ff20b06300ada74c60d3a763ba6a3b
Anu3shka0 <a.nushka1312t@gmai.com>
2025-02-14 01:02:51 +0530

    Initial commit with 2 Qr code

---
+++ NewQrcode.py
@@ -0,0 +1,10 @@
+import qrcode 
+from PIL import Image
+
+qr = qrcode.QRCode(version = 2, error_correction = qrcode.constants.ERROR_CORRECT_H, box_size = 5, border = 12 )
+qr.add_data("www.linkedin.com/in/anushka-tiwari-016187324")
+qr.make(fit = True)
+img = qr.make_image(fill_color = "red", back_color = "black")
+img.save("linkedin1.png")
+img.show()
+print("QR code generated successfully")
